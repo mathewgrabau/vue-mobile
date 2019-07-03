46961a110eea338ac4a6bba081fa6f6d1c35fba1
Mathew Grabau <mgrabau@mathewgrabau.ca>
2019-07-03 15:23:29 -0500

    In progress, currently working on routing and vuex storage.

---
+++ src/components/NewsList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <v-container fluid grid-list-lg>
+            <v-layout row wrap>
+                <v-flex xs12 v-for="newsArticle in newsArticles" :key="newsArticle.id">
+                    <v-card>
+                        <v-container fluid grid-list-lg>
+                            <v-layout row>
+                                <v-flex xs9>
+                                    <div>
+                                        <div class="subheading font-weight-medium">
+                                            Vue.js is absolute rockstar of the Javascript ecosystem.
+                                        </div>
+                                        <div>
+                                            Jan 22, 2019
+                                        </div>
+                                    </div>
+                                </v-flex>
+                                <v-flex xs3>
+                                    <v-img contain :src="`/thumbnails/${newsArticle.baseImageName}.png`" 
+                                        :srcset="`/thumbnails/${newsArticle.baseImageName}.png 1x, /thumbnails/${newsArticle.baseImageName}@2x.png 2x`" 
+                                        height="72px"
+                                        transition="false">
+                                    </v-img>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { NewsArticle } from '../types';
+
+@Component
+export default class NewsList extends Vue {
+    @Prop({ required: true }) newsArticles!: NewsArticle[]; // exclamation point = don't worry about the property not being set in code (binding only will cause TypeScript compiler to complain)
+}    
+
+</script>
+
--- src/views/MyFavourites.vue
+++ src/views/MyFavourites.vue
@@ -1,6 +1,29 @@
 <template>
-    <v-container>
-    <h1 class="headline">[[My Favourties]]</h1>
-        Content is coming to this mobile app very soon!
-  </v-container>
+    <div>
+    <NewsList :newsArticles="newsArticles"></NewsList>
+  </div>
 </template>
+
+<script lang="ts">
+import {Component, Vue } from 'vue-property-decorator';
+import newsService from "../services/newsService";
+import NewsList from "../components/NewsList.vue";
+import { ArticleType, NewsArticle } from '../types';
+
+@Component({
+    components: {
+        NewsList,
+    }
+})
+export default class TopStories extends Vue {
+    public newsArticles: NewsArticle[] = [];
+
+    mounted() {
+      // TODO how to filter the right settings here?
+        newsService.getArticlesByType(ArticleType.TopStory)
+            .then((newsArticles: NewsArticle[]) => {
+                this.newsArticles = newsArticles;
+            });
+    }
+}
+</script>
